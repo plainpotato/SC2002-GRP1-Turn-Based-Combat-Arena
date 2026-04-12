f97e913bcb23413ca28e07894e7e967f4c580f93
darylhui <79079102+darylhui@users.noreply.github.com>
2026-04-12 21:06:17 +0800

    update on combatant

--- src/model/combatants/Combatant.java
+++ src/model/combatants/Combatant.java
@@ -1,5 +1,22 @@
 package model.combatants;
 
-public class Combatant {
+// import model.actions.Action;
+// import model.effects.Status;
 
+
+public interface Combatant {
+    String getName();
+
+    int getHp();
+    int getMaxHp();
+    int getAttack();
+    int getDefense();
+    int getSpeed();
+
+    void setHp(int hp);
+    void setAttack(int attack);
+    void setDefense(int defense);
+
+    boolean isAlive();
+    boolean isEliminated();
 }
---
+++ src/model/items/Potion.java
@@ -0,0 +1,32 @@
+package model.items;
+
+import model.combatants.Combatant;
+
+import java.util.List;
+
+public class Potion implements Item {
+    private static final int MAX_HEAL_AMOUNT = 100;
+    private boolean consumed = false;
+
+    @Override
+    public String getName() {
+        return "Potion";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Restores 100 HP";
+    }
+
+    @Override public boolean isConsumed() { return consumed; }
+
+    @Override
+    public void use(Combatant user, List<Combatant> allCombatants) [
+        int before = user.getCurrentHP();
+        user.heal(MAX_HEAL_AMOUNT);
+        int after = user.getCurrentHP();
+        consumed = true;
+        System.out.printf("%s used a Potion! HP %d -> %d (+%d).%n", user.getName(), before, after, after - before);
+    ]
+    
+}
