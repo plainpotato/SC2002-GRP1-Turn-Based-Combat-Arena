fb5b4b962286d7366778f4f51ffcaf1d54fab0e4
plainpotato <109460797+plainpotato@users.noreply.github.com>
2026-04-15 13:59:22 +0800

    Completed abstract combatant logic

--- src/model/combatants/AbstractCombatant.java
+++ src/model/combatants/AbstractCombatant.java
@@ -52,4 +52,86 @@ public abstract class AbstractCombatant implements Combatant {
         return speed; 
     }
 
+    @Override
+    public void setHp(int hp) {
+        this.hp = Math.max(0, Math.min(hp, maxHp));
+    }
+
+    @Override public void setAttack(int attack) { 
+        this.attack = attack; 
+    }
+    @Override public void setDefense(int defense) { 
+        this.defense = defense; 
+    }
+
+    @Override public boolean isAlive() { 
+        return hp > 0; 
+    }
+
+    @Override public boolean isEliminated() { 
+        return !isAlive(); 
+    }
+
+    @Override
+    public void addStatusEffect(StatusEffect effect) {
+        statusEffects.removeIf(e -> e.getClass().equals(effect.getClass()));
+        statusEffects.add(effect);
+        effect.onApply(this);
+    }
+
+    @Override
+    public void removeStatusEffect(Class<? extends StatusEffect> effectType) {
+        Iterator<StatusEffect> it = statusEffects.iterator();
+        while (it.hasNext()) {
+            StatusEffect e = it.next();
+            if (e.getClass().equals(effectType)) {
+                e.onExpire(this);
+                it.remove();
+            }
+        }
+    }
+
+    @Override
+    public List<StatusEffect> getStatusEffects() {
+        return new ArrayList<>(statusEffects);
+    }
+
+    @Override
+    public boolean hasStatusEffect(Class<? extends StatusEffect> effectType) {
+        return statusEffects.stream().anyMatch(e -> e.getClass().equals(effectType));
+    }
+
+    @Override
+    public void tickStatusEffects() {
+        Iterator<StatusEffect> it = statusEffects.iterator();
+        while (it.hasNext()) {
+            StatusEffect e = it.next();
+            e.tick(this);
+            if (e.isExpired()) {
+                e.onExpire(this);
+                it.remove();
+            }
+        }
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        setHp(hp - Math.max(0, damage));
+    }
+
+    @Override
+    public void heal(int amount) {
+        setHp(hp + amount);
+    }
+
+    @Override
+    public void onRoundEnd() {
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s [HP: %d/%d | ATK: %d | DEF: %d | SPD: %d]",
+                name, hp, maxHp, attack, defense, speed);
+    }
 }
